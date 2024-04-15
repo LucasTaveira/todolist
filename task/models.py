0df49e4d1376c14d6eef3824236c07300637c903
LucasTaveira1 <lucas.taveira-ext@parceirodaconstrucao.com.br>
2024-04-15 11:19:39 -0300

    create new model taks

--- task/models.py
+++ task/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    STATUS_OPTIONS = {
+        "O": "Open",
+        "C": "Closed",
+    }
+    title = models.CharField(
+        verbose_name="Titulo da Tarefa", max_length=50, blank=False, null=False)
+    description = models.TextField(
+        verbose_name="Descrição", blank=True, null=True)
+    status = models.BooleanField(
+        verbose_name="Status", choices=STATUS_OPTIONS, default="O",
+        help_text="O para Open, C para Closed")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.id} - {self.title} - {self.STATUS_OPTIONS[self.status]}"
