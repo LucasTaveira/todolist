98deca15d3c2233c53264003bba4d01913ff6837
LucasTaveira1 <lucas.taveira-ext@parceirodaconstrucao.com.br>
2024-04-16 18:26:23 -0300

    redis working

--- task/views.py
+++ task/views.py
@@ -1,4 +1,8 @@
 from rest_framework import viewsets
+from django.utils.decorators import method_decorator
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.views.decorators.cache import cache_page
 
 from .models import Task
 from .serializers import TaskSerializer
@@ -6,3 +10,10 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    
+    @action(detail=False, methods=['get'], url_path='list-cache')
+    @method_decorator(cache_page(60))
+    def list_cache(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
