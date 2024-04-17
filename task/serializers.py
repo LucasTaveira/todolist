c847273c4572513dc3339eeb7c97b6de43f3e736
LucasTaveira1 <lucas.taveira-ext@parceirodaconstrucao.com.br>
2024-04-17 10:21:39 -0300

    last updates

--- task/serializers.py
+++ task/serializers.py
@@ -9,4 +9,6 @@ class TaskSerializer(serializers.ModelSerializer):
             'title',
             'description',
             'status',
+            'created_at',
+            'updated_at',
         )
